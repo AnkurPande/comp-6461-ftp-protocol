541905f68c4286775d66bbd40bc1fffb1434ed57
ankur.pandey111@gmail.com <ankur.pandey111@gmail.com@aedbfaef-4938-c4bf-4ea4-220b79f479af>
2014-10-06 21:04:09 +0000

    Bytes Overloading fixed

--- server/Assignment1Server/Assignment1Server/Server.cpp
+++ server/Assignment1Server/Assignment1Server/Server.cpp
@@ -64,15 +64,15 @@ DWORD WINAPI clientThread(LPVOID lpParam)
 		recv(currentClient, filename, 20, 0);
 		ifstream myfile;
 		char filecontent[100];
-
+		int something = 0;
 		myfile.open(filename, ios::in | ios::binary);
 		if (myfile.is_open())
 		{
 			while (!myfile.eof())
 			{
 				myfile.read(filecontent, sizeof(filecontent));
-				//cout << sizeof(filecontent) << endl;
-				if ((ibytessent = send(currentClient, filecontent, sizeof(filecontent), 0)) == SOCKET_ERROR)
+				something =myfile.gcount();
+				if ((ibytessent = send(currentClient, filecontent, something, 0)) == SOCKET_ERROR)
 				{
 					throw "error in send in server program while sending data \n";
 				}
@@ -80,24 +80,24 @@ DWORD WINAPI clientThread(LPVOID lpParam)
 				{
 					memset(filecontent, 0, sizeof (filecontent));
 				}
-			}
-			sprintf(szbuffer, "\r\n");
-			ibufferlen = strlen(szbuffer);
-			if ((ibytessent = send(currentClient, szbuffer, ibufferlen, 0)) == SOCKET_ERROR)
-				throw "error in send in server program while sending data \n";
-			else
-			{
-				cout << "File transferred Successfully";
-			}
+			}//while loop
+			//sprintf(szbuffer, "\r\n");
+			//ibufferlen = strlen(szbuffer);
+			//if ((ibytessent = send(currentClient, szbuffer, ibufferlen, 0)) == SOCKET_ERROR)
+			//throw "error in send in server program while sending data \n"; 
+		}//inner if	
+		else
+		{
+			cout << "File transferred Successfully";
 		}
-	}
+
+	}//outer If
 	//Check whether client requested for a Put operation.(Upload to server)
 	if (action[0] == 'P')
 	{
 		recv(currentClient, filename, 20, 0);
 		char filecontent[100];
 		int ibytesrecv = 0;
-
 		ofstream myFile(filename, ios::out | ios::binary);
 		while ((ibytesrecv = recv(currentClient, filecontent, sizeof(filecontent), 0))>0)
 		{
@@ -120,7 +120,7 @@ int main(void)
 	{
 		if (WSAStartup(0x0202, &wsadata) != 0)
 		{
-			cout << "Error in starting WSAStartup()\n"<<WSAGetLastError;
+			cout << "Error in starting WSAStartup()\n" << WSAGetLastError;
 		}
 		else
 		{
@@ -188,7 +188,7 @@ int main(void)
 
 			//create thread and pass the client and return back to accept new client connection.
 			// For every new client request a new dedicated thread is created for the client.
-			
+
 			CreateThread(NULL, 0, clientThread, (LPVOID)s1, 0, &thread);
 
 		}//while loop
